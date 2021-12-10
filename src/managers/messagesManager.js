794107f151752884332e70a6135e1cf62deb9c76
Artem Lukianets <gramsvotn@gmail.com>
2021-12-10 16:48:37 +0200

    Useless manager removed

--- src/managers/messagesManager.js
+++
@@ -1,9 +0,0 @@
-export function messagesManager() {
-  const messages = JSON.parse(
-    localStorage.getItem(process.env.MESSAGES_STORAGE_NAME),
-  );
-
-  if (!messages) {
-    localStorage.setItem(process.env.MESSAGES_STORAGE_NAME, JSON.stringify([]));
-  }
-}
